2256267aa564425789e7354a4e3672980c6c8007
kiwm <pedromaltapedrozo@gmail.com>
2021-11-05 22:32:37 -0300

    js base

---
+++ main.js
@@ -0,0 +1,26 @@
+function shuffle_board() {
+
+}
+
+function init_var() {
+
+}
+
+function print_board() {
+
+}
+
+function win() {
+
+}
+
+function next_move() {
+
+}
+
+function update_board(move) {
+
+}
+
+
+
